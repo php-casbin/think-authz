f5be02f687e8d320cdfe760942fc698c0604d7e9
张航 <100103431@qq.com>
2020-10-21 10:38:51 +0800

    Update Rule.php
    设置字段信息,减少数据库请求.

--- src/model/Rule.php
+++ src/model/Rule.php
@@ -11,6 +11,21 @@ use think\contract\Arrayable;
 class Rule extends Model implements Arrayable
 {
     /**
+     * 设置字段信息
+     *
+     * @var array
+     */
+    protected $schema = [
+        'id'    => 'int',
+        'ptype' => 'string',
+        'v0'    => 'string',
+        'v1'    => 'string',
+        'v2'    => 'string',
+        'v3'    => 'string',
+        'v4'    => 'string',
+        'v5'    => 'string',
+    ];
+    /**
      * 架构函数
      * @access public
      * @param array $data 数据
