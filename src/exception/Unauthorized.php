2daf43e81ea2ce67fbb50fa40b260c5ddbc90474
Jon <leeqvip@gmail.com>
2024-03-22 23:51:23 +0800

    fix: add unauthorized message

--- src/exception/Unauthorized.php
+++ src/exception/Unauthorized.php
@@ -2,6 +2,17 @@
 
 namespace tauthz\exception;
 
-class Unauthorized extends \Exception
+use think\exception\HttpException;
+
+class Unauthorized extends HttpException
 {
+    /**
+     * Create a new exception instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct(403, '此操作未经授权');
+    }
 }
